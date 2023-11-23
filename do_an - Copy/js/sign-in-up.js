b5bbcb3753181b4846dff94d52815c8f4ee7e048
ThaiDuy504 <thaibuileduythai13@gmail.com>
2023-11-23 19:05:13 +0700

    update login eff

--- do_an - Copy/js/sign-in-up.js
+++ do_an - Copy/js/sign-in-up.js
@@ -21,59 +21,58 @@ function closeOption() {
 }
 
 registerLink.addEventListener("click", () => {
-    login.style.display = "none";
-    signup.style.display = "block";
+  login.style.display = "none";
+  signup.style.display = "block";
 });
 
 loginLink.addEventListener("click", () => {
-    signup.style.display = "none";
-    login.style.display = "block";
+  signup.style.display = "none";
+  login.style.display = "block";
 });
 
 btnPopup.addEventListener("click", () => {
-    // wrapper.classList.add("active-popup");
-    wrapper.style.animation="wrapper-in 2s forwards";
+  wrapper.classList.add("active-popup");
+  //   wrapper.style.animation = "wrapper-in 2s forwards";
 });
 
-btn_yes.addEventListener("click",()=>{
-    opt_box.style.animation="box-out 2s";
-    wrapper.style.animation="wrapper-in 2s forwards";
-    // wrapper.classList.add("active-popup");
+btn_yes.addEventListener("click", () => {
+  //   opt_box.style.animation = "box-out 2s";
+  //   wrapper.style.animation = "wrapper-in 2s forwards";
+  wrapper.classList.add("active-popup");
 });
 
 Iconclose.addEventListener("click", () => {
-    wrapper.classList.remove("active-popup");
-    wrapper.style.animation="wrapper-out 2s";
+  wrapper.classList.remove("active-popup");
 });
 
 function checkInputLength(input) {
-    if (input.value.length > 10) {
-        input.value = input.value.slice(0, 10);
-    }
+  if (input.value.length > 10) {
+    input.value = input.value.slice(0, 10);
+  }
 }
 
 const usernameInput = document.querySelector("#checkaccount");
 
 usernameInput.addEventListener("input", (event) => {
-    // Lấy giá trị của input
-    const username = event.target.value;
+  // Lấy giá trị của input
+  const username = event.target.value;
 
-    // Cắt bỏ giá trị nếu vượt quá 8 ký tự
-    if (username.length > 9) {
-        event.target.value = username.substring(0, 9);
-        errorSpan.textContent = "Tên đăng nhập không vượt qua 10 ký tự";
-    } else {
-        // Xóa nội dung của thẻ span
-        errorSpan.textContent = "";
-    }
+  // Cắt bỏ giá trị nếu vượt quá 8 ký tự
+  if (username.length > 9) {
+    event.target.value = username.substring(0, 9);
+    errorSpan.textContent = "Tên đăng nhập không vượt qua 10 ký tự";
+  } else {
+    // Xóa nội dung của thẻ span
+    errorSpan.textContent = "";
+  }
 });
 
 const isLoggedIn = false;
 
 // Khi đăng nhập thành công
 function onLoginSuccess() {
-    isLoggedIn = true;
-    localStorage.setItem("isLoggedIn", isLoggedIn);
+  isLoggedIn = true;
+  localStorage.setItem("isLoggedIn", isLoggedIn);
 }
 
 // Tạo nút đăng xuất
@@ -81,6 +80,6 @@ const logoutButton = document.querySelector("#logout");
 
 // Khi người dùng click vào nút đăng xuất
 logoutButton.addEventListener("click", () => {
-    isLoggedIn = false;
-    localStorage.setItem("isLoggedIn", isLoggedIn);
+  isLoggedIn = false;
+  localStorage.setItem("isLoggedIn", isLoggedIn);
 });

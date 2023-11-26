7d9f95b44f2a86445590355f5b6bb80ebf8ab284
tongthanhdat009 <gamingthanhdat@gmail.com>
2023-11-26 21:16:48 +0700

    thêm chức năng sửa thông tin người dùng

--- do_an/js/user_infor.js
+++ do_an/js/user_infor.js
@@ -38,7 +38,103 @@ checkBill.addEventListener('click', () => {
 
 editInfor.addEventListener('click', () => {
     altPage.style.display="none";
-    rightContent.innerHTML="<h1>CHỈNH SỬA THÔNG TIN TÀI KHOẢN</h1>";
-});
+    rightContent.innerHTML=`<h1 class="user-infor-title">CHỈNH SỬA THÔNG TIN TÀI KHOẢN</h1>
+    <h2> Đổi Họ Và Tên</h2>
+    <div id="nameForm">
+        <form>
+            <label for="nameInput"> Họ và tên : </label>
+            <input autocomplete="on" type="text" id="newName" placeholder="Nhập tên cần thay đổi" required>
+            <button class="all_alt_btn" type="submit" id="update-name">Cập nhật</button>
+        </form>
+    </div>
+    <h2>Đổi Mật khẩu</h2>
+    <div id="passwordForm">
+        <form>
+            <label for="currentPassword">Mật khẩu hiện tại:</label>
+            <input autocomplete="on" type="password" id="currentPassword" required placeholder="nhập mật khẩu hiện tại">
+            </br>
+            <label for="newPassword">Mật khẩu mới:</label>
+            <input autocomplete="on" type="password" id="newPassword" required placeholder="nhập mật khẩu mới">
+            </br>
+            <label for="confirmPassword">Xác nhận mật khẩu mới:</label>
+            <input autocomplete="on" type="password" id="confirmPassword" required placeholder="nhập mật khẩu để xác nhận">
+            </br>
+            <button class="all_alt_btn" type="submit">Đổi Mật khẩu</button>
+        </form>
+    </div>
+    <h2>Thông Tin Địa Chỉ</h2>
+    <div id="addressForm">
+        <form>
+            <label for="newAddress">Địa chỉ mới:</label>
+            <input autocomplete="on" type="text" id="newAddress" required placeholder="nhập địa chỉ cần thay đổi"></textarea>
+            <button class="all_alt_btn" type="submit">Thay Đổi Địa Chỉ</button>
+        </form>
+    </div>
+    <h2>Số Điện Thoại</h2>
+    <div id="phoneNumberForm">
+        <form>
+            <label for="phoneNumberForm"> Đổi số điện thoại: </label>
+            <input autocomplete="on" type="text" id="newPhoneNumber" placeholder="Nhập số điện thoại cần thay đổi" required>
+            <button class="all_alt_btn" type="submit" id="updatePhoneNumber">Cập nhật</button>
+        </form>
+    </div>
+    <h2>Địa Chỉ Email</h2>
+    <div id="emailForm">
+        <form>
+            <label for="newEmail"> Đổi email: </label>
+            <input autocomplete="on" type="text" id="newEmail" placeholder="Nhập email cần thay đổi" required>
+            <button class="all_alt_btn" type="submit" id="updateEmail">Cập nhật</button>
+        </form>
+    </div>`;
 
+    //đổi tên
+    var nameUpdateInput=document.querySelector("#newName");//tên cần cập nhật
+    var nameForm = document.getElementById("nameForm");
+    nameForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        console.log(nameUpdateInput.value);
+        if(nameUpdateInput.value.length > 256 || nameUpdateInput.value.length < 4){
+            alert("Họ và tên không hợp lệ");
+        }
+        else{
+            currentUser.userName=nameUpdateInput.value.length;
+        }
+    });
+
+    //đổi mật khẩu
+    var oldPass = document.querySelector("#currentPassword"); // nhập mật khẩu hiện tại
+    var newPass = document.querySelector("#newPassword");// nhập mật khẩu mới
+    var acptPass = document.querySelector("#confirmPassword");// nhập xác nhận mật khẩu
+    var passwordForm = document.getElementById("passwordForm");
+    passwordForm.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+        console.log(oldPass.value);
+        console.log(newPass.value);
+        console.log(acptPass.value);
+    });
+
+    //đổi địa chỉ
+    var newAddress = document.querySelector("#newAddress");// nhập địa chỉ mới
+    var addressForm = document.getElementById("addressForm");
+    addressForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        console.log(newAddress.value);
+    });
+    
+    //đổi số điện thoại
+    var newPhoneNumber = document.querySelector("#newPhoneNumber");// nhập số điện thoại mới
+    var phoneNumberForm = document.querySelector("#phoneNumberForm");
+    phoneNumberForm.addEventListener("submit",function(e) {
+        e.preventDefault();
+        console.log(newPhoneNumber.value);
+    })
+
+    //đổi email
+    var newEmail = document.querySelector("#newEmail"); //nhập địa chỉ email mới
+    var emailForm = document.querySelector("#emailForm");
+    emailForm.addEventListener("submit",function(e) {
+        e.preventDefault();
+        console.log(newEmail.value);
+    })
+});
 console.log(currentUser);
